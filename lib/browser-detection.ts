d9f719c6320f62344aaf2d3297c367aff0b2e2cf
Mitch Downey <mitchldowney@gmail.com>
2024-06-01 21:08:29 -0500

    Update browser-detection.ts

--- lib/browser-detection.ts
+++ lib/browser-detection.ts
@@ -1,3 +1,7 @@
 const checkIfiOSWebBrowser = () => {
   return /iPad|iPhone|iPod/.test(navigator.userAgent)
 }
+
+const checkIfDesktopBrowser = () => {
+  return !(/Mobi|Android/i.test(navigator.userAgent) || /iPad|iPhone|iPod/.test(navigator.userAgent))
+}
