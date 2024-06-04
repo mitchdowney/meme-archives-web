ed523278861bcd5aa4cd48ab5471fad1dee1c2d8
Mitch Downey <mitchldowney@gmail.com>
2024-06-04 17:02:04 -0500

    Add createCopyableBlobPromise

--- lib/clipboard.ts
+++ lib/clipboard.ts
@@ -1,22 +1,27 @@
 export async function copyImageToClipboard(image: HTMLImageElement) {
   try {
     if (navigator.clipboard && navigator.clipboard.write) {
-      const canvas = document.createElement('canvas')
-      canvas.width = image.naturalWidth
-      canvas.height = image.naturalHeight
-      const ctx = canvas.getContext('2d')
-      if (ctx) {
-        ctx.drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight)
-        canvas.toBlob((blob) => {
-          if (blob) {
-            navigator.clipboard.write([
-              new ClipboardItem({ 'image/png': blob })
-            ])
-          }
-        }, 'image/png')
-      }
+      const copyableBlobPromise = createCopyableBlobPromise(image)
+      await navigator.clipboard.write([new ClipboardItem({ 'image/png': copyableBlobPromise })])
     }
   } catch (error) {
     console.error(error)
   }
+}
+
+async function createCopyableBlobPromise(image: HTMLImageElement) {
+  return new Promise((resolve, reject)=>{
+    try {
+      const canvas = document.createElement('canvas')
+      canvas.width = image.naturalWidth
+      canvas.height = image.naturalHeight
+      const ctx = canvas.getContext('2d')
+      if (!ctx)
+        return reject('Error loading image')
+      ctx.drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight)
+      canvas.toBlob(e=>e ? resolve(e) : reject('Error converting image to blob'), 'image/png')
+    } catch (e) {
+      return reject(e)
+    }
+  })
 }
