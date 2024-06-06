34c19c028222d5cbaeebb67e3170aa84757058ac
Mitch Downey <mitchldowney@gmail.com>
2024-06-06 17:36:28 -0500

    Add to editor rotate, remove, send to back, pull to front

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -4,9 +4,10 @@ import { Rnd } from 'react-rnd'
 import Image from '@/components/Image'
 import styles from '@/styles/Editor.module.css'
 import FAIcon from '@/components/FAIcon'
-import { faRightLeft, faRotateRight } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown, faArrowUp, faRightLeft, faRotateRight, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 type InsertedImage = {
+  id: number
   src: string
   width: number
   height: number
@@ -19,12 +20,16 @@ export default function Editor() {
   const mainImage = 'https://d13jp9qoi0cn61.cloudfront.net/78.png'
   const overlayImages = ['https://d13jp9qoi0cn61.cloudfront.net/79.png']
   const [insertedImages, setInsertedImages] = useState<InsertedImage[]>([])
+  const [isRotating, setIsRotating] = useState(false)
+  const [startX, setStartX] = useState(0)
+  const [rotatingImageIndex, setRotatingImageIndex] = useState(-1)
 
   const handleImageClick = (img: HTMLImageElement) => {
     const aspectRatio = img.naturalWidth / img.naturalHeight
     const width = Math.min(120, img.naturalWidth)
     const height = width / aspectRatio
     setInsertedImages([...insertedImages, {
+      id: Math.floor(Math.random() * 1000000) + 1,
       src: img.src,
       width,
       height,
@@ -36,7 +41,7 @@ export default function Editor() {
 
   const handleRotateClick = (event: any, index: number) => {
     event.stopPropagation()
-    setInsertedImages(insertedImages.map((img, i) => i === index ? { ...img, rotation: (img.rotation + 90) % 360 } : img))
+    setInsertedImages(insertedImages.map((img, i) => i === index ? { ...img, rotation: Math.round((img.rotation + 45) / 90) * 90 % 360 } : img))
   }
 
   const handleFlipXClick = (event: any, index: number) => {
@@ -53,8 +58,52 @@ export default function Editor() {
     }
   }
 
+  // Define event handlers for the rotate-bar
+  const handleRotateMouseDown = (event: React.MouseEvent, index: number) => {
+    setIsRotating(true)
+    setStartX(event.clientX)
+    setRotatingImageIndex(index)
+  }
+
+  const handleRotateMouseMove = (event: React.MouseEvent) => {
+    if (!isRotating || rotatingImageIndex === -1) return
+    const dx = event.clientX - startX    
+    const adjustedAngle = dx * 4
+    setStartX(event.clientX) // Update startX for the next mouse move event
+    setInsertedImages(insertedImages.map((img, i) => i === rotatingImageIndex ? { ...img, rotation: img.rotation + adjustedAngle } : img))
+  }
+
+  const handleRotateMouseUp = () => {
+    setIsRotating(false)
+    setRotatingImageIndex(-1)
+  }
+
   const onResize = (index: number, newWidth: number, newHeight: number) => {
     setInsertedImages(insertedImages.map((img, i) => i === index ? { ...img, width: newWidth, height: newHeight } : img))
+  }
+
+  const handleSendBackOneLevelClick = (index: number) => {
+    if (index > 0) {
+      const newImages = [...insertedImages]
+      const temp = newImages[index]
+      newImages[index] = newImages[index - 1]
+      newImages[index - 1] = temp
+      setInsertedImages(newImages)
+    }
+  }
+
+  const handlePullForwardOneLevelClick = (index: number) => {
+    if (index < insertedImages.length - 1) {
+      const newImages = [...insertedImages]
+      const temp = newImages[index]
+      newImages[index] = newImages[index + 1]
+      newImages[index + 1] = temp
+      setInsertedImages(newImages)
+    }
+  }
+
+  const handleRemoveImage = (index: number) => {
+    setInsertedImages(insertedImages.filter((_, i) => i !== index))
   }
 
   const handleDownload = () => {
@@ -93,7 +142,7 @@ export default function Editor() {
             />
             {insertedImages.map((image, index) => (
               <Rnd
-                key={index}
+                key={image.id}
                 size={{ width: image.width, height: image.height }}
                 onResize={(e, direction, ref, delta, position) => {
                   onResize(index, ref.offsetWidth, ref.offsetHeight)
@@ -104,6 +153,7 @@ export default function Editor() {
                 onFocus={() => handleFocus(index)}
                 onBlur={(event: any) => handleBlur(event, index)}
                 tabIndex={0}
+                cancel='.rotate-bar'
               >
                 {image.focus && (
                   <div className={styles['inserted-image-buttons']}>
@@ -129,7 +179,41 @@ export default function Editor() {
                       onClick={(event) => {
                         handleFlipXClick(event, index)
                       }}
-                      title='Rotate right'
+                      title='Flip horizontally'
+                    />
+                    <FAIcon
+                      activeColor='#333'
+                      buttonWrapperName={styles['inserted-image-button-wrapper']}
+                      buttonClassName={styles['inserted-image-button']}
+                      className={styles['inserted-image-button']}
+                      color='#777'
+                      icon={faArrowUp}
+                      onClick={(event) => {
+                        handleSendBackOneLevelClick(index)
+                      }}
+                      title='Send back one level'
+                    />
+                    <FAIcon
+                      activeColor='#333'
+                      buttonWrapperName={styles['inserted-image-button-wrapper']}
+                      buttonClassName={styles['inserted-image-button']}
+                      className={styles['inserted-image-button']}
+                      color='#777'
+                      icon={faArrowDown}
+                      onClick={(event) => {
+                        handlePullForwardOneLevelClick(index)
+                      }}
+                      title='Pull forward one level'
+                    />
+                    <FAIcon
+                      activeColor='#333'
+                      buttonWrapperName={styles['inserted-image-button-wrapper']}
+                      buttonClassName={styles['inserted-image-button']}
+                      className={styles['inserted-image-button']}
+                      color='#777'
+                      icon={faTrash}
+                      onClick={() => handleRemoveImage(index)}
+                      title='Remove image'
                     />
                   </div>
                 )}
@@ -142,6 +226,17 @@ export default function Editor() {
                   style={{ transform: `rotate(${image.rotation}deg) scaleX(${image.flipX ? '-1' : '1'})` }}
                   title='Inserted image'
                 />
+                {image.focus && (
+                  <div
+                    className={`${styles['rotate-bar']} rotate-bar`}
+                    onMouseDown={(event) => handleRotateMouseDown(event, index)}
+                    onMouseMove={handleRotateMouseMove}
+                    onMouseUp={handleRotateMouseUp}
+                    onMouseLeave={handleRotateMouseUp}
+                  >
+                    <div className={styles['rotate-indicator']} />
+                  </div>
+                )}
               </Rnd>
             ))}
           </div>
