39bd93e54089d69a177664164a6936922133ed2c
Mitch Downey <mitchldowney@gmail.com>
2025-01-24 21:28:19 -0600

    Fix isServerSideReq in apiRequest

--- services/apiRequest.ts
+++ services/apiRequest.ts
@@ -3,7 +3,7 @@ import axios, { AxiosRequestConfig } from 'axios'
 export const apiRequest = (config: AxiosRequestConfig<any>, isServerSideReq?: boolean) => {
   return axios.request({
     baseURL: isServerSideReq
-      ? process.env.NEXT_PUBLIC_API_BASE_URL
+      ? `http://${process.env.NEXT_PUBLIC_INTERNAL_API_BASE_URL}:${process.env.NEXT_PUBLIC_INTERNAL_API_PORT}`
       : process.env.NEXT_PUBLIC_API_BASE_URL,
     ...config
   })
