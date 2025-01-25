f676b897b00d99892830281d9175e646124eecf7
Mitch Downey <mitchldowney@gmail.com>
2025-01-24 18:11:29 -0600

    Fix lint error

--- pages/[imageIdOrSlug].tsx
+++ pages/[imageIdOrSlug].tsx
@@ -429,7 +429,7 @@ export default function ImagePage({ initialImage, userInfo }: Props) {
                                         </>
                                       </Button>
                                     )
-                                  } */}
+                                  }
                                 </div>
                               )
                             } */}
