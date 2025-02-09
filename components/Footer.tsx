be61a6f495b96cd3412f301917e8b797b45f10d7
Mitch Downey <mitchldowney@gmail.com>
2025-02-09 15:02:10 -0600

    Load footer after one second

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from 'react'
 import { configText } from '@/lib/constants/configurables'
 import styles from '@/styles/components/Footer.module.css'
 
 export default function Footer() {
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true)
+    }, 1000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (!isVisible) {
+    return null
+  }
+
   return (
     <div className={styles['footer']}>
       <p className={styles['disclaimer']}>
