e23ed10669b11ad6132a10342fc9cb689413f3bd
Mitch Downey <mitchldowney@gmail.com>
2025-05-15 21:26:05 -0500

    Update configurables for croc

--- lib/constants/configurables.tsx
+++ lib/constants/configurables.tsx
@@ -26,18 +26,18 @@ export const pageRules: PageRules = {
   // Enable or disable pages 
   gallery: true,
   memes: false,
-  videos: false,
+  videos: true,
   artists: true,
   collections: false,
   stickers: false,
-  whitepaper: true,
-  roadmap: true,
-  resources: true,
-  memeMaker: true,
+  whitepaper: false,
+  roadmap: false,
+  resources: false,
+  memeMaker: false,
   admin: true,
   // If you'd like the home page to load the gallery instead of the splash page,
   // set homePageIsGallery to true, and rename the pages/art.tsx file to pages/index.tsx.
-  homePageIsGallery: false,
+  homePageIsGallery: true,
   // Hide the image type dropdown filter in the gallery
   disableImageTypes: true,
   // Hide the radio buttons under the search bar
@@ -45,30 +45,30 @@ export const pageRules: PageRules = {
   // Hide all of the other buttons and center the ViewTypeSelector under the search bar
   centerViewTypeSelector: true,
   // Prevent border images from being uploaded or displayed
-  disableBorderImages: false,
+  disableBorderImages: true,
   // Set the default preview crop style
   defaultPreviewCropStyle: 'middle'
 }
 
 export const configImages = {
-  useDeprecatedNoBorderImageName: true
+  useDeprecatedNoBorderImageName: false
 }
 
 // Set to empty string to disable
 export const configCharts = {
   birdeyeUrl: '',
   coingeckoUrl: '',
-  dexscreenerUrl: 'https://dexscreener.com/solana/8x9c5qa4nvakKo5wHPbPa5xvTVMKmS26w4DRpCQLCLk3',
+  dexscreenerUrl: 'https://dexscreener.com/solana/cyncsecwzylwrx8zwq6ivj7ca6pl2csfatfi238wcqu5',
   dextoolsUrl: ''
 }
 
 // Set to empty string to disable
 export const configSocials = {
   farcasterUrl: '',
-  telegramUrl: 'https://t.co/OjMn6rdbaU',
+  telegramUrl: 'https://t.me/CrocCat69',
   discordUrl: '',
-  twitterUrl: 'https://twitter.com/mspaintsol',
-  twitterHandle: '@mspaintsol',
+  twitterUrl: 'https://twitter.com/croccatofficial',
+  twitterHandle: '@croccatofficial',
   instagramUrl: '',
   youtubeUrl: '',
   tiktokUrl: '',
@@ -76,8 +76,8 @@ export const configSocials = {
 }
 
 export const configText = {
-  appName: '$PAINT',
-  disclaimer: 'This website is for entertainment and informational purposes only, not financial or investment advice. \$PAINT, this website, and its related social media accounts are not affiliated with, sponsored by, or endorsed by Microsoft.'
+  appName: '$CROC',
+  disclaimer: 'This website is for entertainment and informational purposes only, not financial or investment advice.'
 }
 
 export const configMemeMaker = {
@@ -96,7 +96,7 @@ export const configPageText = {
     metaDescription: ''
   },
   art: {
-    metaTitle: `${configText.appName} - Art Gallery`,
+    metaTitle: `${configText.appName} - Gallery`,
     metaDescription: ''
   },
   artist: {
@@ -161,34 +161,34 @@ export type ConfigStickerSet = {
 export const configStickerSets: ConfigStickerSet[] = [
   {
     href: 'https://t.me/addstickers/PAINTmojiPACK',
-    title: '$PAINT-mojis'
+    title: '$CROC-mojis'
   },
   {
     href: 'https://t.me/addstickers/TICKERISPAINT',
-    title: '$PAINT Set #1'
+    title: '$CROC Set #1'
   },
   {
     href: 'https://t.me/addstickers/TICKERISPAINT2',
-    title: '$PAINT Set #2'
+    title: '$CROC Set #2'
   },
   {
     href: 'https://t.me/addstickers/TICKERISPAINT3',
-    title: '$PAINT Set #3'
+    title: '$CROC Set #3'
   },
   {
     href: 'https://t.me/addstickers/TICKERISPAINT4',
-    title: '$PAINT Set #4'
+    title: '$CROC Set #4'
   },
   {
     href: 'https://t.me/addstickers/TICKERISPAINT5',
-    title: '$PAINT Set #5'
+    title: '$CROC Set #5'
   },
   {
     href: 'https://t.me/addstickers/AnnoyingPaintPack',
-    title: 'Annoying $PAINT Pack'
+    title: 'Annoying $CROC Pack'
   },
   {
     href: 'https://t.me/addstickers/PAINTHASLEGS',
-    title: '$PAINT Has Legs'
+    title: '$CROC Has Legs'
   }
 ]
